5d9f5666375b4c52da770172d95e0b90a1493d0e
mrtxee <mrtxee@bk.ru>
2022-12-27 15:24:02 +0300

    Update settings.py

--- thbwa/settings.py
+++ thbwa/settings.py
@@ -29,7 +29,8 @@ CORS_ORIGIN_ALLOW_ALL = DEBUG
 # CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000', 'http://localhost:3000', 'http://127.0.0.1:3001', 'http://localhost:3001',
-    'http://127.0.0.1:8000', 'http://localhost:8000', 'http://127.0.0.1:8001', 'http://localhost:8001'
+    'http://127.0.0.1:8000', 'http://localhost:8000', 'http://127.0.0.1:8001', 'http://localhost:8001',
+    'mrtxee.pythonanywhere.com'
 ]
 
 #CSRF_COOKIE_NAME = "XSRF-TOKEN"
